cf02b9131efdfd143d6d85732d972ab9cfb25b73
Maxim Merzlikin <merzlikinmax@mail.ru>
2020-03-18 16:46:26 +0700

    Ad more KY

--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 int main(){
 	int a=1,b=2,c=8;
-	cout<<"KY "<<a+b<<endl;
-	a=9;
-	b=4;
-	cout<<"a*b*c="<<a*b*c<<endl<<"Hi Max, Micha";
+	cout<<"KYKYKYKYKYKY "<<a+b<<endl;
+	a=10;
+	b=9;
+	cout<<"a*b*c="<<a*b*c<<endl<<"Hi Max Merzlikin, Micha Kim";
 	return 0;
 }
-
